2119a89a7b1f3ccf4599038348fddcf73c28690a
Yasuaki MORITA <zousandazou@gmail.com>
2017-01-17 10:06:22 +0900

    test

--- omega-test.sh
+++ omega-test.sh
@@ -7,3 +7,27 @@ EOS
 diff $res - << EOS
 selfApp:(forall X.X->X)->forall X.X->X
 EOS
+
+./omega > $res << EOS
+Nat:*;
+zero:Nat;
+succ:Nat->Nat;
+id = fun X. fun x:X. x;
+id Nat zero;
+double = fun X. fun f:X->X. fun a:X. f (f a);
+doubleNat = double Nat;
+double Nat (fun x:Nat. succ(succ(x))) (succ(succ(succ(zero))));
+doubleNatArrowNat = double (Nat->Nat);
+EOS
+diff $res - << EOS
+Nat:*
+zero:Nat
+succ:Nat->Nat
+id:forall X.X->X
+zero
+double:forall X.(X->X)->X->X
+doubleNat:(Nat->Nat)->Nat->Nat
+succ(succ(succ(succ(succ(succ(succ zero))))))
+doubleNatArrowNat:((Nat->Nat)->Nat->Nat)->(Nat->Nat)->Nat->Nat
+EOS
+
